70fa3d0ecb86d73835f2251a6e61e230dc92fa96
curioussavage <curioussavage@protonmail.com>
2016-12-29 23:14:40 -0800

    move js to new lib directory

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var makeApp = require('./lights.js');
-var startServer = require('./server.js');
-var db = require('./db.js');
+var makeApp = require('./lib/lights.js');
+var startServer = require('./lib/server.js');
+var db = require('./lib/db.js');
 
 console.log('starting')
 
 var savedLights = db.find({}, function(err, lights) {
-  if (err) { console.log(err); return; }
-  var app = makeApp(lights);
-  startServer(app);
+    if (err) { console.log(err); return; }
+    var app = makeApp(lights);
+    startServer(app);
 });
-
